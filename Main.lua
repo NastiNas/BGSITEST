8a608353936dc8c68434db597bbc753614bced86
NastiNas <91199786+NastiNas@users.noreply.github.com>
2025-04-20 00:26:53 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -85,7 +85,7 @@ local function autoHop()
 end
 
 local function start()
-    print("Started")
+    print("Started, Actively Searching For".. TargetRift)
 	repeat task.wait() until game:IsLoaded()
 	task.wait(5)
 	local found = checkForRift()
